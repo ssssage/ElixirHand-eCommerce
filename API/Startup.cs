c1a4607795adca0ac513a3cd34b74d59636ec59e
rudph1 <rudph1@ac.uk>
2020-05-03 04:03:57 +0100

    Removed HTTPSRedirection

--- API/Startup.cs
+++ API/Startup.cs
@@ -6,38 +6,44 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace API {
+namespace API
+{
 
-    public class Startup {
+    public class Startup
+    {
 
         private readonly IConfiguration _config;
 
-        public Startup (IConfiguration config) 
+        public Startup(IConfiguration config)
         {
             _config = config;
         }
 
-       // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices (IServiceCollection services) {
-            services.AddControllers ();
-            services.AddDbContext<ElixirHandShopContext>(x => 
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddDbContext<ElixirHandShopContext>(x =>
             x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure (IApplicationBuilder app, IWebHostEnvironment env) {
-            if (env.IsDevelopment ()) {
-                app.UseDeveloperExceptionPage ();
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection ();
 
-            app.UseRouting ();
 
-            app.UseAuthorization ();
+            app.UseRouting();
 
-            app.UseEndpoints (endpoints => {
-                endpoints.MapControllers ();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
             });
         }
     }
