3178c4ebf303b352eaa63908139b8c4604eb3d1a
rudph1 <rudph1@ac.uk>
2020-05-17 00:54:11 +0100

    PictureUrl path fixed by adding custome value resolver automapper

--- API/Helpers/MappingProfiles.cs
+++ API/Helpers/MappingProfiles.cs
@@ -11,7 +11,8 @@ namespace API.Helpers
             //Object Mapping Configuration
             CreateMap<Product, ProductToReturnDto>()
                 .ForMember(d => d.ProductBrand, o => o.MapFrom(s => s.ProductBrand.Name))
-                .ForMember(d => d.ProductType, o => o.MapFrom(s => s.ProductType.Name));
+                .ForMember(d => d.ProductType, o => o.MapFrom(s => s.ProductType.Name))
+                .ForMember(d => d.PictureUrl, o => o.MapFrom< ProductUrlResolver>());
                 
         }
     }
---
+++ API/Helpers/ProductUrlResolver.cs
@@ -0,0 +1,30 @@
+﻿using API.Dtos;
+using AutoMapper;
+using Core.Entities;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Helpers
+{
+    public class ProductUrlResolver : IValueResolver<Product, ProductToReturnDto, string>
+    {
+        private readonly IConfiguration _config;
+
+        public ProductUrlResolver(IConfiguration config)
+        {
+            _config = config;
+        }
+        public string Resolve(Product source, ProductToReturnDto destination,
+            string destMember, ResolutionContext context)
+        {
+            if (!string.IsNullOrEmpty(source.PictureUrl))
+            {
+                return _config["ApiUrl"] + source.PictureUrl;
+            }
+            return null;
+        }
+    }
+}
