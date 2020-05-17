c621df50155d9c111582b0b931ce85f9b4c7654e
rudph1 <rudph1@ac.uk>
2020-05-17 22:25:11 +0100

    ApiResponse added to generate consistent error response from API

---
+++ API/Errors/ApiResponse.cs
@@ -0,0 +1,16 @@
+﻿
+namespace API.Errors
+{
+    public class ApiResponse
+    {
+        public ApiResponse(int statusCode, string message)
+        {
+            StatusCode = statusCode;
+            Message = message;
+        }
+
+        public int StatusCode { get; set; }
+
+        public string Message { get; set; }
+    }
+}
