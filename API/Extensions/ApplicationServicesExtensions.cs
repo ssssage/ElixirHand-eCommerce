ef3a288bdc3f8f3f91554ac60908e4f18543ac2b
ElixirHand <rudph1@hotmail.com>
2020-05-22 21:17:58 +0100

    Extension startup and housekeeping by added SwaggerServiceExtensions, ApplicationServicesExtensions

---
+++ API/Extensions/ApplicationServicesExtensions.cs
@@ -0,0 +1,41 @@
+﻿using API.Errors;
+using Core.Interfaces;
+using Infrastructure.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using System.Linq;
+
+namespace API.Extentions
+{
+    public static class ApplicationServicesExtensions
+    {
+         public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddScoped<IProductRepository, ProductRepository>();
+            
+            services.AddScoped(typeof(IGenericRepository<>), (typeof(GenericRepository<>)));
+
+            services.Configure<ApiBehaviorOptions>(options =>
+            {
+                options.InvalidModelStateResponseFactory = actionContext =>
+                {
+                    var errors = actionContext.ModelState
+                        .Where(e => e.Value.Errors.Count > 0)
+                        .SelectMany(x => x.Value.Errors)
+                        .Select(x => x.ErrorMessage).ToArray();
+
+                    var errorResponse = new ApiValidationResponse
+                    {
+                        Errors = errors
+                    };
+
+                    return new BadRequestObjectResult(errorResponse);
+                };
+            });
+
+            return services;
+        }
+
+    }
+   
+}
--- API/Startup.cs
+++ API/Startup.cs
@@ -1,18 +1,14 @@
-using API.Errors;
+using API.Extensions;
+using API.Extentions;
 using API.Helpers;
 using API.Middleware;
 using AutoMapper;
-using Core.Interfaces;
 using Infrastructure.Data;
-using Infrastructure.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.OpenApi.Models;
-using System.Linq;
 
 namespace API
 {
@@ -30,34 +26,14 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddScoped<IProductRepository, ProductRepository>();
-            services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
             services.AddAutoMapper(typeof(MappingProfiles));
             services.AddControllers();
             services.AddDbContext<ElixirHandShopContext>(x =>
             x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
 
-            services.Configure<ApiBehaviorOptions>(options =>
-            {
-                options.InvalidModelStateResponseFactory = actionContext =>
-                {
-                    var errors = actionContext.ModelState
-                      .Where(e => e.Value.Errors.Count > 0)
-                      .SelectMany(x => x.Value.Errors)
-                      .Select(x => x.ErrorMessage).ToArray();
+            services.AddApplicationServices();
 
-                    var errorResponse = new ApiValidationResponse
-                    {
-                        Errors = errors
-                    };
-                    return new BadRequestObjectResult(errorResponse);
-                };
-            });
-
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "ElixirHand API Project", Version = "v1" });
-            });
+            services.AddSwaggerDocumentation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -73,8 +49,7 @@ namespace API
             app.UseStaticFiles();
             app.UseAuthorization();
 
-            app.UseSwagger();
-            app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "ElixirHand API v1"); });
+            app.UseSwaggerDocumentation();
 
             app.UseEndpoints(endpoints =>
             {
