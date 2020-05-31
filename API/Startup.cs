b34fe46928da05f7aede20cfd78ffb789fd312cf
ElixirHand <rudph1@hotmail.com>
2020-05-31 23:08:02 +0100

    Cors Enabled

--- API/Startup.cs
+++ API/Startup.cs
@@ -34,6 +34,14 @@ namespace API
             services.AddApplicationServices();
 
             services.AddSwaggerDocumentation();
+
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -47,6 +55,9 @@ namespace API
 
             app.UseRouting();
             app.UseStaticFiles();
+
+            app.UseCors("CorsPolicy");
+
             app.UseAuthorization();
 
             app.UseSwaggerDocumentation();
