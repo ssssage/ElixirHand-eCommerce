06dc10c22cf0e9a26ae33fa1228416121e567c77
Sageer <rudph6@gmail.com>
2021-03-18 12:03:29 +0000

    new store component generated

--- FrontendApp/ClientApp/src/app/store/store.module.ts
+++ FrontendApp/ClientApp/src/app/store/store.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { StoreComponent } from './store.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [StoreComponent],
   imports: [
     CommonModule
   ]
