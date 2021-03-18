ad5dc29bc7cf724dcd281a7bc9c8125d4bb97e39
Sageer <rudph6@gmail.com>
2021-03-18 16:23:02 +0000

    brand and product types api data called

--- FrontendApp/ClientApp/src/app/store/store.service.ts
+++ FrontendApp/ClientApp/src/app/store/store.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { IBrand } from '../shared/brand';
 import { InterfacePaging } from '../shared/Interfaces/paging';
+import { IProductType } from '../shared/productType';
 
 @Injectable({
   providedIn: 'root'
@@ -12,5 +14,13 @@ export class StoreService {
 
   getProducts() {
     return this.http.get<InterfacePaging>(this.baseUrl + 'products?pageSize=20')
+  }
+
+  getBrands() {
+    return this.http.get<IBrand>(this.baseUrl + 'products/brands')
+  }
+  
+  getProductTypes() {
+    return this.http.get<IProductType>(this.baseUrl + 'products/types')
   }
 }
