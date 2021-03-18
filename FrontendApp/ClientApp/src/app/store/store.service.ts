a41b5984f952991f98bb4788a271d61c1ed3f2d3
Sageer <rudph6@gmail.com>
2021-03-18 12:58:07 +0000

    api called in service

--- FrontendApp/ClientApp/src/app/store/store.service.ts
+++ FrontendApp/ClientApp/src/app/store/store.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { InterfacePaging } from '../shared/Interfaces/paging';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StoreService {
+  baseUrl = 'https://localhost:5001/api/'
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getProducts() {
+    return this.http.get<InterfacePaging>(this.baseUrl + 'products')
+  }
 }
