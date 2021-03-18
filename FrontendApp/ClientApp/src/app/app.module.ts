9cd28e433cf262310a9ce9b889ea7af7bfb079d1
Sageer <rudph6@gmail.com>
2021-03-18 12:19:09 +0000

    exported TopNavBarComponent

--- FrontendApp/ClientApp/src/app/app.module.ts
+++ FrontendApp/ClientApp/src/app/app.module.ts
@@ -5,18 +5,19 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { TopNavBarComponent } from './core/top-nav-bar/top-nav-bar.component';
+import { CoreModule } from './core/core.module';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopNavBarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    HttpClientModule
+    HttpClientModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- FrontendApp/ClientApp/src/app/core/core.module.ts
+++ FrontendApp/ClientApp/src/app/core/core.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TopNavBarComponent } from './top-nav-bar/top-nav-bar.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [TopNavBarComponent],
   imports: [
     CommonModule
-  ]
+  ],
+  exports: [TopNavBarComponent]
 })
 export class CoreModule { }
