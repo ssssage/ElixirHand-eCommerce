0b4db94e4ab60cca997b813604414419a1c7cda6
Sageer <rudph6@gmail.com>
2021-03-22 16:30:35 +0000

    shared component for pagination

--- FrontendApp/ClientApp/src/app/shared/shared.module.ts
+++ FrontendApp/ClientApp/src/app/shared/shared.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PaginationModule } from 'ngx-bootstrap/pagination';
+import { PagingHeaderComponent } from './components/paging-header/paging-header.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [PagingHeaderComponent],
   imports: [
     CommonModule,
     PaginationModule.forRoot()
   ],
-  exports: [PaginationModule]
+  exports: [
+    PaginationModule,
+    PagingHeaderComponent
+  ]
 })
 export class SharedModule { }
