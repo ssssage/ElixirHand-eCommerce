db94491a5952cf2e793d6f1656bc2d7fd5e8c7d8
Sageer <rudph6@gmail.com>
2021-03-24 21:05:01 +0000

    not found and server error functioning

--- FrontendApp/ClientApp/src/app/app-routing.module.ts
+++ FrontendApp/ClientApp/src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NotFoundComponent } from './core/not-found/not-found.component';
+import { ServerErrorComponent } from './core/server-error/server-error.component';
 import { TestErrorComponent } from './core/test-error/test-error.component';
 import { HomeComponent } from './home/home.component';
 
@@ -9,6 +11,8 @@ import { HomeComponent } from './home/home.component';
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'test-error', component: TestErrorComponent },
+  { path: 'server-error', component: ServerErrorComponent },
+  { path: 'not-found', component: NotFoundComponent },
   { path: 'store', loadChildren: () => import('./store/store.module').then(mod => mod.StoreModule), data: { breadcrumb: 'Store' } },
   { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
--- FrontendApp/ClientApp/src/app/core/core.module.ts
+++ FrontendApp/ClientApp/src/app/core/core.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { TopNavBarComponent } from './top-nav-bar/top-nav-bar.component';
 import { RouterModule } from '@angular/router';
 import { TestErrorComponent } from './test-error/test-error.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { ServerErrorComponent } from './server-error/server-error.component';
 
 
 
 
 @NgModule({
-  declarations: [TopNavBarComponent, TestErrorComponent],
+  declarations: [TopNavBarComponent, TestErrorComponent, NotFoundComponent, ServerErrorComponent],
   imports: [
     CommonModule,
     RouterModule
