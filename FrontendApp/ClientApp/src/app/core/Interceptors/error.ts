63a574c0fd2c82521f0dec453422dafb11c2bd33
Sageer <rudph6@gmail.com>
2021-07-13 19:55:52 +0100

    Toast Notifications Added

--- FrontendApp/ClientApp/src/app/core/Interceptors/error.ts
+++ FrontendApp/ClientApp/src/app/core/Interceptors/error.ts
@@ -3,15 +3,22 @@ import { Observable, throwError } from 'rxjs';
 import { Router, NavigationExtras } from '@angular/router';
 import { Injectable } from '@angular/core';
 import { catchError } from 'rxjs/operators';
+import { ToastrService } from 'ngx-toastr';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
-    constructor(private router: Router) {}
+  constructor(private router: Router, private toastr: ToastrService) {}
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         return next.handle(req).pipe(
             catchError(error => {
-                if (error) {           
+              if (error) {
+                if (error.status === 400) {
+                  this.toastr.error(error.error.message, error.error.statusCode);
+                  }
+                if (error.status === 401) {
+                  this.toastr.error(error.error.message, error.error.statusCode);
+                  }
                     if (error.status === 404) {
                         this.router.navigateByUrl('/not-found');
                     }
--- FrontendApp/ClientApp/src/app/core/core.module.ts
+++ FrontendApp/ClientApp/src/app/core/core.module.ts
@@ -6,6 +6,7 @@ import { TestErrorComponent } from './test-error/test-error.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { ServerErrorComponent } from './server-error/server-error.component';
 import { SectionHeaderComponent } from './section-header/section-header.component';
+import { ToastrModule, ToastNoAnimation, ToastNoAnimationModule } from 'ngx-toastr';
 import { BreadcrumbModule } from 'xng-breadcrumb';
 
 
@@ -15,8 +16,12 @@ import { BreadcrumbModule } from 'xng-breadcrumb';
   declarations: [TopNavBarComponent, TestErrorComponent, NotFoundComponent, ServerErrorComponent, SectionHeaderComponent],
   imports: [
     CommonModule,
-    RouterModule,
-    BreadcrumbModule
+    RouterModule,   
+    BreadcrumbModule,
+    ToastNoAnimationModule.forRoot({
+      positionClass: 'toast-bottom-right',
+      preventDuplicates: true
+    })
   ],
   exports: [
     TopNavBarComponent,
