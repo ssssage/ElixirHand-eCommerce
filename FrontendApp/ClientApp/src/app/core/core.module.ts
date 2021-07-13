aa82d444780ca5977828403ff42b718439c37963
Sageer <rudph6@gmail.com>
2021-07-13 13:45:28 +0100

    breadcrumb added

--- FrontendApp/ClientApp/src/app/core/core.module.ts
+++ FrontendApp/ClientApp/src/app/core/core.module.ts
@@ -6,6 +6,7 @@ import { TestErrorComponent } from './test-error/test-error.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { ServerErrorComponent } from './server-error/server-error.component';
 import { SectionHeaderComponent } from './section-header/section-header.component';
+import { BreadcrumbModule } from 'xng-breadcrumb';
 
 
 
@@ -14,7 +15,8 @@ import { SectionHeaderComponent } from './section-header/section-header.componen
   declarations: [TopNavBarComponent, TestErrorComponent, NotFoundComponent, ServerErrorComponent, SectionHeaderComponent],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    BreadcrumbModule
   ],
   exports: [
     TopNavBarComponent,
