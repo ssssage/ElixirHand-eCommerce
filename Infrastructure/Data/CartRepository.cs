6eabe4c3908868a39ae80846574be177b3125c05
Sageer <rudph6@gmail.com>
2021-07-28 10:45:36 +0100

    Redis configured CartRepository

--- Infrastructure/Data/CartRepository.cs
+++ Infrastructure/Data/CartRepository.cs
@@ -1,14 +1,19 @@
 ﻿using Core.Entities;
 using Core.Interfaces;
+using StackExchange.Redis;
 using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Infrastructure.Data
 {
     public class CartRepository : ICartRepository
     {
+        private readonly IDatabase _database;
+
+        public CartRepository(IConnectionMultiplexer redis)
+        {
+            _database = redis.GetDatabase();
+        }
         public Task<bool> DeleteCartAsync(string cartId)
         {
             throw new NotImplementedException();
