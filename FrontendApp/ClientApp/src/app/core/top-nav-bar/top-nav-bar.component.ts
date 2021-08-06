70701fc49257d82a954ce56d2a89f968b45d1dc8
Sageer <rudph6@gmail.com>
2021-08-06 16:04:06 +0100

    nav bar cart item counter works

--- FrontendApp/ClientApp/src/app/core/top-nav-bar/top-nav-bar.component.ts
+++ FrontendApp/ClientApp/src/app/core/top-nav-bar/top-nav-bar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { CartService } from '../../cart/cart.service';
+import { ICart } from '../../shared/Interfaces/cart';
 
 @Component({
   selector: 'app-top-nav-bar',
@@ -7,9 +10,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TopNavBarComponent implements OnInit {
 
-  constructor() { }
+  cart$: Observable<ICart>;
+
+  constructor(private cartService: CartService) { }
+
 
   ngOnInit(): void {
+    this.cart$ = this.cartService.cart$;
   }
 
 }
